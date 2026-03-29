682cef56dbfeb51297cead5fb7b1c2e5d76dbda8
DevelopFarahat <mohamedfarahat366@gmail.com>
2026-03-30 01:13:29 +0200

    Serialize result

--- src/main/java/com/jenkins/job/firstjenkinsjob/service/DailyApiTask.java
+++ src/main/java/com/jenkins/job/firstjenkinsjob/service/DailyApiTask.java
@@ -32,7 +32,7 @@ public class DailyApiTask implements CommandLineRunner {
         result.add(response);
         // Serialize to JSON string
         String json = mapper.writeValueAsString(result);
-        System.out.print(List.of());
+        System.out.print(json);
 
         // Exit cleanly so Jenkins knows the job finished
         System.out.flush();
